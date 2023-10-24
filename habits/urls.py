b6b5f7cb3bd9a2be3031e18de4ddc06761fed58c
Uliana Semyonova <i@ulianasemyonova.ru>
2023-10-24 20:23:18 +0500

    Create urls

---
+++ habits/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from habits.apps import HabitsConfig
+from habits.views import (HabitListAPIView, PublicHabitListAPIView, HabitCreateAPIView, HabitUpdateAPIView,
+                          HabitDeleteAPIView)
+
+app_name = HabitsConfig.name
+
+
+urlpatterns = [
+    path("list/", HabitListAPIView.as_view(), name="habit-list"),
+    path("public_list/", PublicHabitListAPIView.as_view(), name="habit-public-list"),
+    path("create/", HabitCreateAPIView.as_view(), name="habit-create"),
+    path("update/<int:pk>", HabitUpdateAPIView.as_view(), name="habit-update"),
+    path("delete/<int:pk>", HabitDeleteAPIView.as_view(), name="habit-delete"),
+]
